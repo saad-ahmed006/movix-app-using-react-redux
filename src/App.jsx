da044ec6f5634195b1aa7e80df27d2fb1119dcc9
saad-ahmed006 <muhammadsaad368444@gmail.com>
2023-03-31 15:19:39 +0500

    other-component-modify

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useEffect } from "react"
 import { fetchDataFromApi } from "./Utils/api"
 import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration } from "./Store/HomeSlice"
+import { BrowserRouter as Router ,Routes,Route,} from "react-router-dom";
+import Home from "./Pages/Home/Home"
+import Explore from "./Pages/Explore/Explore"
+import Error from "./Pages/Error/Error"
+import Detail from "./Pages/Detail/Detail"
+import SearchResult from "./Pages/SearchResult/SearchResult"
+import Header from "./Component/Header/Header"
+import Footer from "./Component/Footer/Footer";
 
 function App() {
   const dispatch = useDispatch()
@@ -13,12 +21,23 @@ function App() {
   const ApiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
-    dispatch(getApiConfiguration(res))
+      dispatch(getApiConfiguration(res))
     })
   }
   return (
     <>
       <h1>This is react js project </h1>
+      <Router>
+        <Header/>
+        <Routes>
+      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/:mediaType/:id" element={<Detail/>} />
+      <Route exact path="/search/:query" element={<SearchResult/>} />
+      <Route exact path="/explore/:mediaType" element={<Explore/>} />
+      <Route exact path="*" element={<Error/>} />
+        </Routes>
+        <Footer/>
+    </Router>
     </>
   )
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroSection from './HeroSection/HeroSection'
+
+export default function Home() {
+  return (
+    <div>
+        <HeroSection/>
+    </div>
+  )
+}
