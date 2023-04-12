7677ac93c054a2e510d5db661551883b9e8a970f
saad-ahmed006 <muhammadsaad368444@gmail.com>
2023-04-12 19:19:48 +0500

    toprated-componet

--- src/Pages/Home/TopRated/TopRated.jsx
+++ src/Pages/Home/TopRated/TopRated.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import '../Home.scss'
+import SwitchTabs from '../../../Component/SwitchTabs/SwitchTabs'
+import useFetch from '../../../Hook/useFetch'
+import Carasul from '../../../Component/Carasul/Carasul'
+import ContentWrapper from '../../../Component/contentWrapper/ContentWrapper'
 export default function TopRated() {
-  return (
-    <div>TopRated</div>
-  )
+    const [endpoint, setEndpoint] = useState("movie")
+    const { data, loading } = useFetch(`/${endpoint}/top_rated`)
+    // console.log(data);
+    const onTabChange = (tab) => {
+        setEndpoint(tab === 'Movies' ? 'movie' : 'tv')
+    }
+    return (
+        <div className="carouselSection">
+            <ContentWrapper>
+                <span className="carouselTitle">Top Rated</span>
+                <SwitchTabs data={["Movies", "Tv Shows"]} onTabChange={onTabChange} />
+            </ContentWrapper>
+            <ContentWrapper>
+                <Carasul data={data?.results} loading={loading} endpoint={endpoint}/>
+            </ContentWrapper>
+
+
+        </div>
+    )
+
 }
 
